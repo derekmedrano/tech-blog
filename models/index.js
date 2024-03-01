2c82578cedd55e8ca5a3f2267dd48feb51440781
Derek Medrano <derekjmedrano@gmail.com>
2024-03-01 16:20:23 -0600

    set up more of the model files, mostly index

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'cascade',
+});
+
+module.exports = { User, Post, Comment};
