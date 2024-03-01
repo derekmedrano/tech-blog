03d67ee4fa259b86487ad5e07b422be258246496
Derek Medrano <derekjmedrano@gmail.com>
2024-03-01 16:11:19 -0600

    rewrote server so i can use handlebars and session    :)

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
+// required packages
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('sequelize');
+const session = require('express-session');
+const expHb = require('express-handlebars'); // expHb >>> expressHandlebars
+const routes = require('./controllers');
 
+//sequelize and session imported
+const sequelize = require('sequelize');
+const sessionStore = require('connect-session-sequelize')(session.Store);
+
+//allows express to be used by server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const sess = { 
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new sessionStore({
+        db: sequelize
+    })
+
+};
+
+//allows session to be used by server
+app.use(session(sess));
+
+//allows handlebars for use by server
+const hb = expHb.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//app middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => 
-{ app.listen(PORT, () => { console.log(`App listening on port ${PORT}!`); 
-    }); 
+
+
+
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}!`));
 });
+
