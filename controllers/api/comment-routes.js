48dbe52545cea389cafcad684c7953d56af882c8
Derek Medrano <derekjmedrano@gmail.com>
2024-03-04 19:24:04 -0600

    added find all for posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
-const { Comment } = require("../../models");
-const sequelize = require("../../config/connection");
-const withAuth = require("../../utils/auth");
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 
 router.get("/", (req, res) => {
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,28 @@
-const router = require("express").Router();
-const { Comment } = require("../../models");
-const sequelize = require("../../config/connection");
-const withAuth = require("../../utils/auth");
+const router = require('express').Router();
+const { Post } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: ['id', 'title', 'body'],
+      include: [{
+        model: User,
+        attributes: ['name']
+    },
+    {
+        model: Comment,
+        attributes: ['id', 'body', 'post_id', 'user_id'],
+        include: {
+            model: User,
+            attributes: ['name']
+        }
+    }
+  ]
+    })
+      .then((postData) => res.json(postData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
