ec9cbcf570592069973a2b883ef19f740f6a0174
Derek Medrano <derekjmedrano@gmail.com>
2024-03-04 19:16:09 -0600

    added comment update route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -5,36 +5,60 @@ const withAuth = require("../../utils/auth");
 
 
 router.get("/", (req, res) => {
-  Comment.findAll({})
-    .then((commentData) => res.json(commentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    Comment.findAll({})
+        .then((commentData) => res.json(commentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get("/:id", (req, res) => {
-  Comment.findAll({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((commentData) => res.json(commentData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    Comment.findAll({
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((commentData) => res.json(commentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', async (req, res) => {
     try {
-      const commentData = await Comment.create({
-        body: req.body.body
-      });
-      res.status(200).json(commentData);
+        const commentData = await Comment.create({
+            body: req.body.body
+        });
+        res.status(200).json(commentData);
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
-      }
-    });
+    }
+});
+
+router.put("/:id", withAuth, (req, res) => {
+    Comment.update(
+        {
+            body: req.body.body,
+        },
+        {
+            where: {
+                id: req.params.id,
+            },
+        }
+    )
+        .then((commentData) => {
+            if (!commentData) {
+                res.status(404).json({ message: "No comment exists with this id!" });
+                return;
+            }
+            res.json(commentData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
