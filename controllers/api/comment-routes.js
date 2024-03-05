00d23b68c01431856f467b705d9348cc3fdea755
Derek Medrano <derekjmedrano@gmail.com>
2024-03-04 19:06:51 -0600

    added comment routes without post or update

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
+
+
+router.get("/", (req, res) => {
+  Comment.findAll({})
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Comment.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
